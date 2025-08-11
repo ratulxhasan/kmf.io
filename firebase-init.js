55dcbfd7625f0329f55868bcb9baa90350449426
Md Ratul Hasan <82720031+ratulxhasan@users.noreply.github.com>
2025-08-11 10:05:20 +0600

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -26,3 +26,14 @@ const db = getDatabase(app);
 
 // All simple exports for use in script.js
 export { auth, provider, onAuthStateChanged, signInWithPopup, signOut, db, ref, set, get, update, onValue };
+const firebaseConfig = {
+  apiKey: "AIzaSyDOpfGcOaV6SE3Z6hGrB8k_BNKcII6Psyg",
+  authDomain: "kmfworkers.firebaseapp.com",
+  databaseURL: "https://kmfworkers-default-rtdb.firebaseio.com", // <-- ADD THIS LINE!
+  projectId: "kmfworkers",
+  storageBucket: "kmfworkers.firebasestorage.app",
+  messagingSenderId: "345856300895",
+  appId: "1:345856300895:web:c0b07b90b45f86989b88ea",
+  measurementId: "G-M35E3DJD9V"
+};
+
