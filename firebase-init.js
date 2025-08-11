c17dd7fdf391ce2429a69e476ec5dc5e838eca28
Md Ratul Hasan <82720031+ratulxhasan@users.noreply.github.com>
2025-08-11 09:57:39 +0600

    Update and rename app.js to firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,28 @@
+// firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signInWithPopup, GoogleAuthProvider, signOut } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getDatabase, ref, set, get, update, onValue } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
+
+// Your config
+const firebaseConfig = {
+  apiKey: "AIzaSyDOpfGcOaV6SE3Z6hGrB8k_BNKcII6Psyg",
+  authDomain: "kmfworkers.firebaseapp.com",
+  projectId: "kmfworkers",
+  storageBucket: "kmfworkers.firebasestorage.app",
+  messagingSenderId: "345856300895",
+  appId: "1:345856300895:web:c0b07b90b45f86989b88ea",
+  measurementId: "G-M35E3DJD9V"
+};
+
+// Init Firebase
+const app = initializeApp(firebaseConfig);
+
+// Auth
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+// Database
+const db = getDatabase(app);
+
+// All simple exports for use in script.js
+export { auth, provider, onAuthStateChanged, signInWithPopup, signOut, db, ref, set, get, update, onValue };
